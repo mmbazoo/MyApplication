e30e5350b1530f1aab09c8bfab28c06a641aa508
mmbazoo <mmbazoo@stud.hs-heilbronn.de>
2018-01-01 20:27:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/violainemb/myapplication/HHNStandortFragment.java
@@ -0,0 +1,67 @@
+package com.example.violainemb.myapplication;
+
+
+import android.Manifest;
+import android.app.VoiceInteractor;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HHNStandortFragment extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+
+    public HHNStandortFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_hhnstandort, container, false);
+
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getChildFragmentManager().findFragmentById(R.id.map1);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng sydney = new LatLng(-34,151);
+        MarkerOptions options = new MarkerOptions();
+        options.position(sydney).title("sydney");
+        map.addMarker(options);
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+    }
+}
+
+
+
+
